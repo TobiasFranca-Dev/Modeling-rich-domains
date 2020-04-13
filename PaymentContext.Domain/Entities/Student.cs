789a5d8771dfe8864287fb22ae1edfd36c87a14c
Tobias França <tobias@aobsoft.com.br>
2020-04-13 19:01:15 -0300

    Add Entities

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        private IList<Subscription> _subscriptions;
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            //Cancels all previous subscriptions and sets this as the primary one
+
+            foreach (var sub in _subscriptions)
+            {
+                sub.Inactivate();
+            }
+
+            _subscriptions.Add(subscription);
+        }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AddSubscription()
+        {
+            var subscription = new Subscription(null);
+
+            var student = new Student("Tobias", "França", "12312312312", "email@email.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
